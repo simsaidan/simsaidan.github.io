b5d365a5d74c8a30be3fa406d916be4a96bf7463
Aidan Sims <ans248@cornell.edu>
2023-02-02 18:27:09 -0500

    links

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const button = document.getElementById('go-button');
+const select = document.querySelector('select');
+const selected = select.options[select.selectedIndex].text;
 
 button.addEventListener('click', () => {
-  window.location = 'cnn.com';
+  if (selected === 'Link 1') {
+    window.location = 'cnn.com';
+  } else if (selected === 'Link 2') {
+    window.location = 'https://www.espn.com/';
+  }
 });
