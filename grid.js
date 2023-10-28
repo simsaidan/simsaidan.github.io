8a3e9d4a3687009bb04b0d3334975b5ff2bd7df9
Aidan Sims <ans248@cornell.edu>
2023-10-28 16:16:48 -0400

    starting anew

--- grid.js
+++ grid.js
@@ -1,1 +1,11 @@
-const categories = [["Left Handed"]];
+const categories = [["Left Handed"],
+["Born after 1995", "Born before 1975"],
+["Not from Europe", "From Australia", "From Asia", "From South America", "American", "From Europe"],
+["Won at least 20 titles", "No titles", "Title on All 3 Surfaces", "Unseeded Champion"],
+["Grand Slam Winner", "5+ Slams", "GS Finalist but no GS", "Wimbledon Champion"],
+["Top 5 Ranking"],
+["Played Davis Cup", "Won Davis Cup"],
+["Olympic Medalist", "Played in Olympics"]];
+
+
+
