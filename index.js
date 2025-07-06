74aa4ad8e44fd0af60c7870bcbb0bd228e97b9aa
Aidan Sims <ans248@cornell.edu>
2025-07-05 17:47:13 -0700

    edited globe

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const contactGlobe = Globe()
-  (document.getElementById('miniGlobe'))
-  .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
-  .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
-  .pointColor(() => '#ff5722')
-  .pointAltitude(0.01)
-  .pointRadius(0.25)
-  .pointsData([
-    { lat: 37.7749, lng: -122.4194 }, // SF
-    { lat: 40.7128, lng: -74.0060 }   // NY
-  ]);
+window.addEventListener("DOMContentLoaded", () => {
+  const globe = Globe()
+    (document.getElementById('miniGlobe'))
+    .globeImageUrl(null) // no earth texture
+    .showGlobe(true)
+    .globeColor("rgba(0, 0, 0, 0)") // fully transparent sphere
+    .showGraticules(true)           // optional: lat/lon lines
+    .showAtmosphere(true)
+    .atmosphereColor('#88f')
+    .atmosphereAltitude(0.1)
+    .pointColor(() => '#ff5722')
+    .pointAltitude(0.01)
+    .pointRadius(0.25)
+    .pointsData([
+      { lat: 37.7749, lng: -122.4194 },
+      { lat: 40.7128, lng: -74.0060 }
+    ]);
 
-contactGlobe.controls().autoRotate = true;
-contactGlobe.controls().autoRotateSpeed = 0.5;
+  globe.controls().autoRotate = true;
+  globe.controls().autoRotateSpeed = 0.5;
+});
