2f8f3f87c86e6ff707bef025a26d7a1a2f49e33a
Aidan Sims <ans248@cornell.edu>
2025-08-12 08:30:03 -0700

    added supabase

--- grid.js
+++ grid.js
@@ -1,6 +1,6 @@
 
 
-const { createClient } = supabase;
+const { createClient } = window.supabase;
 const SUPABASE_URL = 'https://fivdlwpvvysahzxtnnej.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpdmRsd3B2dnlzYWh6eHRubmVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4Njc4NzAsImV4cCI6MjA3MDQ0Mzg3MH0.JvzGYMnZMJul2kUxE1hunbGIoOcQ_dfdhAjSb6IOk5w';
 
